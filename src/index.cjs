c55c912ceb5285f7700a1dbdc73c9242d350ffde
Robpol86 <robpol86@gmail.com>
2025-05-10 16:51:57 +0200

    smaller diff

--- src/index.cjs
+++ src/index.cjs
@@ -10,7 +10,7 @@ const { getBasename } = require("./lib.cjs");
  * @param {string} url - URL file path to the loaded file.
  */
 function onFileLoaded(url) {
-    iina.console.log("url", url);
+    iina.console.log("Url", url);
     iina.console.log("basename", getBasename(url));
     iina.core.osd("Starts playing");
 }
