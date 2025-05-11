98dbe0da50525a0d2097ece825bdfa566c015196
Robpol86 <robpol86@gmail.com>
2025-05-11 12:51:23 +0200

    Linting JSON files (#4)
    * eslintPluginPackageJson
    
    * relock
    
    * json lint
    
    * lint json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,24 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginPackageJson from "eslint-plugin-package-json";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import js from "@eslint/js";
+import json from "@eslint/json";
 
 export default defineConfig([
+    globalIgnores(["coverage/"]),
     { languageOptions: { globals: { ...globals.node, iina: true } } },
-    { plugins: { js }, extends: ["js/recommended"] },
+    { files: ["**/*.js", "**/*.cjs", "**/*.mjs"], plugins: { js }, extends: ["js/recommended"] },
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        ignores: ["package.json", "package-lock.json"],
+        ...json.configs.recommended,
+        rules: {
+            "json/sort-keys": "error",
+        },
+    },
     {
         rules: {
             "no-unused-vars": [
@@ -25,4 +38,5 @@ export default defineConfig([
         },
     },
     eslintPluginPrettierRecommended,
+    eslintPluginPackageJson.configs.recommended,
 ]);
