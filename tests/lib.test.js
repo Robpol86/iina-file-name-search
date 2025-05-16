c5d65609472ee6400790a1137990556a4beda213
Robpol86 <robpol86@gmail.com>
2025-05-16 13:43:22 +0200

    test one attempt

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, jest, test } from "@jest/globals";
-import { getFileNameSansExt } from "../src/lib.cjs";
+import { getFileNameSansExt, postMessageAck } from "../src/lib.cjs";
 
 describe("getFileNameSansExt", () => {
     test.each([
@@ -19,8 +19,22 @@ describe("getFileNameSansExt", () => {
 });
 
 describe("postMessageAck", () => {
+    let logs = [];
+    let onMessageRegistered = new Map();
+    let postMessageSent = [];
+    const iina = {
+        console: { log: (message) => logs.push(message) },
+        sidebar: {
+            onMessage: (name, callback) => onMessageRegistered.set(name, callback),
+            postMessage: (name) => postMessageSent.push(name),
+        },
+    };
+
     beforeEach(() => {
         jest.useFakeTimers();
+        logs = [];
+        onMessageRegistered = new Map();
+        postMessageSent = [];
     });
 
     afterEach(() => {
@@ -28,7 +42,22 @@ describe("postMessageAck", () => {
         jest.useRealTimers();
     });
 
-    test.todo("one attempt");
+    test("one attempt", () => {
+        // Send initial message.
+        postMessageAck("sidebar", "test", {}, 100, 1000, iina);
+        expect(logs.pop()).toEqual("postMessageAck(sidebar, test, ...)");
+        expect(postMessageSent.pop()).toEqual("test");
+        // Remote acknowledges.
+        onMessageRegistered.get("test::ack")();
+        onMessageRegistered.delete("test::ack");
+        expect(logs.pop()).toEqual("sidebar acknowledged test message");
+        // Confirm timer cancelled.
+        jest.advanceTimersByTime(2000);
+        expect(logs).toHaveLength(0);
+        expect(onMessageRegistered.size).toEqual(0);
+        expect(postMessageSent).toHaveLength(0);
+    });
+
     test.todo("two attempts");
     test.todo("timeout");
 });
