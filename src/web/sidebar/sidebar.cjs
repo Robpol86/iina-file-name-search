a666721f0866b2677e6ff9e1f6f9c8bb03bbe76c
Robpol86 <robpol86@gmail.com>
2025-05-16 12:01:17 +0200

    radio button changes input

--- src/web/sidebar/sidebar.cjs
+++ src/web/sidebar/sidebar.cjs
@@ -1,3 +1,21 @@
 /**
  * Main javascript file for the plugin's sidebar view.
  */
+
+const radioFileName = document.getElementById("radioFileName");
+const radioRegex = document.getElementById("radioRegex");
+const searchInput = document.getElementById("searchInput");
+
+/**
+ * Replace searchInput text with default value when user selects a radio button.
+ *
+ * User can use this to reset any changes they made to the searchInput field.
+ *
+ * @param {Event} event - Event information.
+ */
+function fillSearchInput(event) {
+    const radio = event.target;
+    searchInput.value = radio.dataset.searchInputValue ?? "";
+}
+radioFileName.addEventListener("change", fillSearchInput);
+radioRegex.addEventListener("change", fillSearchInput);
